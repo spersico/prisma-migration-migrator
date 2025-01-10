b4ad444cb8c8b314f1a2e11437c9824dc2f90def
Santiago Persico <santiagopersico@gmail.com>
2025-01-10 15:44:37 +0100

    start extended test

---
+++ src/tests/cases/extended.ts
@@ -0,0 +1,8 @@
+import type { TestParameters } from '../types.js';
+import base from './base.js';
+
+export default async function main(parameters: TestParameters) {
+  await base(parameters);
+
+  //
+}
