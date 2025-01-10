994133fe9c912ebfd80e6a03f75b35a8b6be15c2
Santiago Persico <santiagopersico@gmail.com>
2025-01-10 13:16:43 +0100

    remove annoying eslint rule

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -43,5 +43,6 @@ export default [{
         "@typescript-eslint/explicit-function-return-type": "off",
         "@typescript-eslint/explicit-module-boundary-types": "off",
         "@typescript-eslint/no-explicit-any": "off",
+        '@typescript-eslint/no-unused-expressions': 'off',
     },
 }];
