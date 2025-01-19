53d90021c9cbfe8c46e266a8360df66baa55b49c
Santiago Persico <santiagopersico@gmail.com>
2025-01-19 18:31:30 +0100

    fix wrong path

--- src/tests/helpers/constants.ts
+++ src/tests/helpers/constants.ts
@@ -2,7 +2,11 @@ import path from 'node:path';
 import { getBaseDirectory } from '../../migrator/directories.js';
 
 const prismaSchemaPath = path.resolve('prisma', 'schema.prisma');
-const knexMigrationsDir = path.join(getBaseDirectory(), 'knex_migrations');
+const knexMigrationsDir = path.join(
+  getBaseDirectory(),
+  'prisma',
+  'knex_migrations',
+);
 
 const baseDBUrl = 'postgres://myuser:mypassword@localhost:5432';
 const password = 'mypassword';
