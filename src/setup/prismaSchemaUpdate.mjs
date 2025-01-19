9b64210ed910c211c5c77a0abba25edff747e976
Santiago Persico <santiagopersico@gmail.com>
2025-01-19 23:02:10 +0100

    restore pg specific difference on schema

--- src/setup/prismaSchemaUpdate.mjs
+++ src/setup/prismaSchemaUpdate.mjs
@@ -15,9 +15,9 @@ const prismaSchemaFile_knexModels = `
 model knex_migrations {
   @@ignore
   id             Int       @id @default(autoincrement())
-  name           String?
+  name           String?   @db.VarChar(255)
   batch          Int?
-  migration_time DateTime?
+  migration_time DateTime? @db.Timestamptz(6)
 }
 
 model knex_migrations_lock {
